b4861fc73a10d3dc20d78cf70e81e9f8cd05df31
xuefengji <jxfvip@sina.com>
2020-01-29 17:19:51 +0800

    docs:update docs

--- demos/oop.py
+++ demos/oop.py
@@ -7,9 +7,23 @@ class Student(object):
     def print_score(self):
         print(self.name,self.__score)
 
+class Animal(object):
+    def __init__(self,name):
+        self.name = name
+
+    def run(self):
+        print('%s is run' %self.name)
+
+class Dog(Animal):
+    def __init__(self):
+        super(Dog, self).__init__('Dog1')
+        # Animal.__init__(Animal,'Dog')
+        # self.name = name
+
+
 if __name__=='__main__':
-    stu = Student('lisa',89)
-    # print(stu.__score)
-    stu.print_score()
-    print(stu.__dir__())
-    print(stu.age)
+    # stu = Student('lisa',89)
+    # stu.print_score()
+    # print(stu.__dir__())
+    dog = Dog()
+    dog.run()
