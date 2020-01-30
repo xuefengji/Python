cdd1c7712932ba90fd84b8099364b9f452f5aea5
xuefengji <jxfvip@sina.com>
2020-01-30 11:27:37 +0800

    docs:update docs

--- demos/Oop高级.py
+++ demos/Oop高级.py
@@ -7,6 +7,17 @@ class Student:
     def show(self,value):
         self._name = value
 
-stu = Student
-stu.show = 'kisa'
-print(stu.show)
+# stu = Student
+# stu.show = 'kisa'
+# print(stu.show)
+
+class Stu(object):
+    def __init__(self):
+        self.name = 'Michael'
+
+    def __str__(self):
+        return 'Student object (name: %s)' % self.name
+
+
+stu = Stu()
+print(stu)
