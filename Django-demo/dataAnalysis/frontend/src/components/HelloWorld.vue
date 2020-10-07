0d464b05ecf93ad5a4ad7fd2ade305beda2dbec7
xuefengji <xuefvip@163.com>
2020-10-07 21:47:26 +0800

    feat: add content

--- Django-demo/dataAnalysis/frontend/src/components/HelloWorld.vue
+++ Django-demo/dataAnalysis/frontend/src/components/HelloWorld.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <span>请选择要解析的日志文件</span>
+    <el-upload
+    class="upload-demo"
+    ref="upload"
+    action="https://jsonplaceholder.typicode.com/posts/"
+    :on-preview="handlePreview"
+    :on-remove="handleRemove"
+    :before-remove="beforeRemove"
+    :auto-upload="false">
+    <!-- <el-button slot="trigger" size="small" type="primary">选取文件</el-button> -->
+    <el-button style="margin-left: 10px;" size="small" type="success" @click="submitUpload">上传到服务器</el-button>
+    <!-- <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div> -->
+  </el-upload>
     
   </div>
 </template>
@@ -12,8 +25,23 @@ export default {
   data() {
     return {
       msg: '简单日志分析平台',
+      // fileList: [{name: 'food.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'}, {name: 'food2.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'}],
     }
-  }
+  },
+    methods: {
+      submitUpload() {
+        this.$refs.upload.submit();
+      },
+      handleRemove(file, fileList) {
+        console.log(file, fileList);
+      },
+      handlePreview(file) {
+        console.log(file);
+      },
+      beforeRemove(file, fileList) {
+        return this.$confirm(`确定移除 ${ file.name }？`);
+      }
+    }
 }
 </script>
 
