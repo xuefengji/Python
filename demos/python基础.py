036b514e59e5f25314bcbaddd7f23d213cf321a7
xuefengji <xuefvip@163.com>
2021-01-06 20:43:57 +0800

    update

--- demos/python基础.py
+++ demos/python基础.py
@@ -1,10 +1,3 @@
-hello = 'I\'m \"ok\"!'
-print(hello)
-print(r'\\\t\\')
-print('''line1
-... line2
-... line3''')
-
-print('hello %s' %"world")
-print('%2d-%02d' % (3, 1))
-print('%.2f' % 3.1415926)
+a = 1
+b = "hello world"
+print(c)
