217d24d96c601d6588d4480654960d3e48b515e7
xuefengji <xuefvip@163.com>
2021-01-23 20:39:27 +0800

    feat: 增加石墨文档下载功能

--- demos/python基础.py
+++ demos/python基础.py
@@ -1,3 +1,39 @@
-a = 60
-print(bin(a))
-print(True == 1)
+# a = 60
+# print(bin(a))
+# print(True == 1)
+
+# a = 1
+# print("函数outer调用之前全局变量a的内存地址： ", id(a))
+# def outer():
+#     a = 2
+#     print("函数outer调用之时闭包外部的变量a的内存地址： ", id(a))
+#     def inner():
+#         nonlocal  a   # 注意这行
+#         a = 3
+#         print("函数inner调用之后闭包内部变量a的内存地址： ", id(a))
+#     inner()
+#     print("函数inner调用之后，闭包外部的变量a的内存地址： ", id(a))
+# outer()
+# print("函数outer执行完毕，全局变量a的内存地址： ", id(a))
+
+
+# a = 10
+# print(id(a))
+# def test():
+#     # global a
+#     a += 1
+#     print(id(a))
+# test()
+
+name ='jack'
+
+def f1():
+    print(name)
+
+def f2():
+    name = 'eric'
+    # def f1():
+     #     print(name)
+    f1()
+
+f2()
---
+++ shimo-auto-download/README.md
@@ -0,0 +1,36 @@
+# shimo-auto-download
+
+## 项目说明
+
+本项目为解决自动下载石墨文档上的文件到本地存档
+
+
+
+## 运行环境
+
++ Python 3.8
++ requests 库版本 2.25.1
++ 可使用 pip install -r requirements.txt 进行安装 requests
+
+
+
+## 项目介绍
+
++ 项目结构
+
+  shimo-auto-download
+  │  .gitignore
+  │  config.py
+  │  README.md
+  │  requirements.txt
+  │  run.py
+
+  
+
+  config.py : 项目的配置文件
+
+  run.py：进行文件下载的文件
+
+  requirements.txt ：安装的库的版本文件
+
+  
